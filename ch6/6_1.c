8643f1a5ddb6cb136edf9e90d05b9909dd269b4e
jing-ge <675324370@qq.com>
2020-10-30 10:33:04 +0800

    finish ch6

---
+++ ch6/6_1.c
@@ -0,0 +1,19 @@
+#include <pwd.h>
+#include <stddef.h>
+#include <string.h>
+
+struct passwd *
+getpwnam(const char *name)
+{
+	struct passwd *ptr;
+	setpwent();
+	while((ptr = getpwent()) != NULL)
+			if(strcmp(name, ptr->pw_name) == 0)
+					break;
+	endpwent();
+	return(ptr);
+}
+int main(void)
+{
+	return 0;
+}
